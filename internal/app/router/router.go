eadacc35c859ed76b13a20a20ce9e5b201c6d734
liuzhichun <liuzhichun@apipost.cn>
2022-08-19 19:11:02 +0800

    test: cros

--- internal/app/router/router.go
+++ internal/app/router/router.go
@@ -12,7 +12,7 @@ import (
 func RegisterRouter(r *gin.Engine) {
 	// cors
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		//AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"POST", "GET", "OPTIONS", "DELETE", "PUT", "PATCH"},
 		AllowHeaders:     []string{"Origin"},
 		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
