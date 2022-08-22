3343e7adcae4814f0141b3a5999427f8ba19f790
liuzhichun <liuzhichun@apipost.cn>
2022-08-22 19:00:14 +0800

    feat: add reports handler

---
+++ internal/pkg/handler/report.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"kp-management/internal/pkg/biz/errno"
+	"kp-management/internal/pkg/biz/response"
+	"kp-management/internal/pkg/dal/rao"
+	"kp-management/internal/pkg/logic/report"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ListReports(ctx *gin.Context) {
+	var req rao.ListReportsReq
+	if err := ctx.ShouldBind(&req); err != nil {
+		response.ErrorWithMsg(ctx, errno.ParamError, err.Error())
+		return
+	}
+
+	reports, total, err := report.ListByTeamID(ctx, req.TeamID, req.Size, (req.Page-1)*req.Size)
+	if err != nil {
+		response.ErrorWithMsg(ctx, errno.MysqlOperFailed, err.Error())
+		return
+	}
+
+	response.SuccessWithData(ctx, rao.ListReportsResp{
+		Reports: reports,
+		Total:   total,
+	})
+}
