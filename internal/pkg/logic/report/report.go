8c1aaadbd1d0d52f4ca369dc56a991480c36983c
liuzhichun <liuzhichun@apipost.cn>
2022-08-23 15:12:59 +0800

    feat: add report list

--- internal/pkg/logic/report/report.go
+++ internal/pkg/logic/report/report.go
@@ -2,6 +2,10 @@ package report
 
 import (
 	"context"
+	"fmt"
+	"time"
+
+	"gorm.io/gen"
 
 	"kp-management/internal/pkg/dal"
 	"kp-management/internal/pkg/dal/query"
@@ -15,9 +19,34 @@ func CountByTeamID(ctx context.Context, teamID int64) (int64, error) {
 	return tx.WithContext(ctx).Where(tx.TeamID.Eq(teamID)).Count()
 }
 
-func ListByTeamID(ctx context.Context, teamID int64, limit, offset int) ([]*rao.Report, int64, error) {
+func ListByTeamID(ctx context.Context, teamID int64, limit, offset int, keyword string, startTimeSec, endTimeSec int64) ([]*rao.Report, int64, error) {
 	tx := query.Use(dal.DB()).Report
-	reports, err := tx.WithContext(ctx).Where(tx.TeamID.Eq(teamID)).
+
+	conditions := make([]gen.Condition, 0)
+	conditions = append(conditions, tx.TeamID.Eq(teamID))
+
+	if keyword != "" {
+		conditions = append(conditions, tx.Name.Like(fmt.Sprintf("%%%s%%", keyword)))
+
+		u := query.Use(dal.DB()).User
+		users, err := u.WithContext(ctx).Where(u.Nickname.Like(fmt.Sprintf("%%%s%%", keyword))).Find()
+		if err != nil {
+			return nil, 0, err
+		}
+
+		if len(users) > 0 {
+			conditions[1] = tx.RunUserID.Eq(users[0].ID)
+			//conditions = append(conditions, tx.RunUserID.Eq(users[0].ID))
+		}
+	}
+
+	if startTimeSec > 0 && endTimeSec > 0 {
+		startTime := time.Unix(startTimeSec, 0)
+		endTime := time.Unix(endTimeSec, 0)
+		conditions = append(conditions, tx.CreatedAt.Between(startTime, endTime))
+	}
+
+	reports, err := tx.WithContext(ctx).Where(conditions...).
 		Order(tx.UpdatedAt.Desc(), tx.CreatedAt.Desc()).
 		Limit(limit).Offset(offset).Find()
 
@@ -25,7 +54,7 @@ func ListByTeamID(ctx context.Context, teamID int64, limit, offset int) ([]*rao.
 		return nil, 0, err
 	}
 
-	cnt, err := tx.WithContext(ctx).Where(tx.TeamID.Eq(teamID)).Count()
+	cnt, err := tx.WithContext(ctx).Where(conditions...).Count()
 	if err != nil {
 		return nil, 0, err
 	}
