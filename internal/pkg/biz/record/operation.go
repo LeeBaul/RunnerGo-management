bf3a7bd0d03ad6acf5e66e944994c8f33c5666d7
liuzhichun <liuzhichun@apipost.cn>
2022-08-26 17:25:31 +0800

    feat: add record func

--- internal/pkg/biz/record/operation.go
+++ internal/pkg/biz/record/operation.go
@@ -17,6 +17,14 @@ func InsertUpdate(ctx context.Context, teamID, userID int64, name string) error 
 	return insert(ctx, teamID, userID, name, consts.OperationCategoryUpdate)
 }
 
+func InsertDelete(ctx context.Context, teamID, userID int64, name string) error {
+	return insert(ctx, teamID, userID, name, consts.OperationCategoryDelete)
+}
+
+func InsertRun(ctx context.Context, teamID, userID int64, name string) error {
+	return insert(ctx, teamID, userID, name, consts.OperationCategoryRun)
+}
+
 func insert(ctx context.Context, teamID, userID int64, name string, category int32) error {
 	return query.Use(dal.DB()).Operation.WithContext(ctx).Create(&model.Operation{
 		TeamID:   teamID,
