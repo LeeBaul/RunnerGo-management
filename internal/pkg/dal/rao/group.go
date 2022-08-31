2f4e1a68f907f0997e5d5484568b557a2874cdb0
liuzhichun <liuzhichun@apipost.cn>
2022-08-31 15:30:19 +0800

    refactor: add group validator

--- internal/pkg/dal/rao/group.go
+++ internal/pkg/dal/rao/group.go
@@ -1,14 +1,14 @@
 package rao
 
 type SaveGroupReq struct {
-	TeamID   int64    `json:"team_id"`
+	TeamID   int64    `json:"team_id" binding:"required,gt=0"`
 	TargetID int64    `json:"target_id"`
 	ParentID int64    `json:"parent_id"`
-	Name     string   `json:"name"`
+	Name     string   `json:"name" binding:"required,max=32"`
 	Method   string   `json:"method"`
 	Sort     int32    `json:"sort"`
 	TypeSort int32    `json:"type_sort"`
-	Version  int32    `json:"version"`
+	Version  int32    `json:"version" binding:"required,gt=0"`
 	Request  *Request `json:"request"`
 	Script   *Script  `json:"script"`
 }
