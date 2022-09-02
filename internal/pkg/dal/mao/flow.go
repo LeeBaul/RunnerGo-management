5eb38f053e867b3621910115be20d7143e29946d
liuzhichun <liuzhichun@apipost.cn>
2022-09-02 16:06:05 +0800

    refactor: update nodes/edges

--- internal/pkg/dal/mao/flow.go
+++ internal/pkg/dal/mao/flow.go
@@ -1,8 +1,13 @@
 package mao
 
+import "go.mongodb.org/mongo-driver/bson"
+
 type Flow struct {
-	SceneID int64  `bson:"scene_id"`
-	TeamID  int64  `bson:"team_id"`
-	Version int32  `bson:"version"`
-	Flows   string `bson:"flows"`
+	SceneID int64 `bson:"scene_id"`
+	TeamID  int64 `bson:"team_id"`
+	Version int32 `bson:"version"`
+	//Flows   string `bson:"flows"`
+	Nodes           bson.Raw `bson:"nodes"`
+	Edges           bson.Raw `bson:"edges"`
+	MultiLevelNodes bson.Raw `bson:"multi_level_nodes"`
 }
--- internal/pkg/dal/rao/scene.go
+++ internal/pkg/dal/rao/scene.go
@@ -39,27 +39,37 @@ type Scene struct {
 }
 
 type SaveFlowReq struct {
-	SceneID int64   `json:"scene_id" binding:"required,gt=0"`
-	TeamID  int64   `json:"team_id" binding:"required,gt=0"`
-	Version int32   `json:"version"`
-	Flows   []*Flow `json:"flows" bson:"flows"`
+	SceneID int64 `json:"scene_id" binding:"required,gt=0"`
+	TeamID  int64 `json:"team_id" binding:"required,gt=0"`
+	Version int32 `json:"version"`
+
+	Nodes           []*Node `json:"nodes"`
+	Edges           []*Edge `json:"edges"`
+	MultiLevelNodes []byte  `json:"multi_level_nodes"`
+}
+
+type Edge struct {
+	Id     string `json:"id"`
+	Source string `json:"source"`
+	Target string `json:"target"`
 }
 
 type SaveFlowResp struct {
 }
 
-type Flow struct {
-	Index     int    `json:"index"`
-	FlowType  string `json:"flow_type"`
-	NextIndex int    `json:"next_index,omitempty"`
+type Node struct {
+	ID      string `json:"id"`
+	Type    string `json:"type"`
+	IsCheck bool   `json:"is_check"`
 
 	// 接口
-	Weight            int  `json:"weight,omitempty"`
-	Mode              int  `json:"mode,omitempty"`
-	ErrorThreshold    int  `json:"error_threshold,omitempty"`
-	ResponseThreshold int  `json:"response_threshold,omitempty"`
-	RequestThreshold  int  `json:"request_threshold,omitempty"`
-	API               *API `json:"api,omitempty"`
+	Weight            int        `json:"weight,omitempty"`
+	Mode              int        `json:"mode,omitempty"`
+	ErrorThreshold    int        `json:"error_threshold,omitempty"`
+	ResponseThreshold int        `json:"response_threshold,omitempty"`
+	RequestThreshold  int        `json:"request_threshold,omitempty"`
+	PercentAge        int        `json:"percent_age,omitempty"`
+	API               *APIDetail `json:"api,omitempty"`
 
 	// 全局断言
 	Assets []string `json:"assets,omitempty"`
@@ -74,20 +84,20 @@ type Flow struct {
 }
 
 // API 接口详情
-type API struct {
-	TargetID    int64     `json:"target_id"`
-	ParentID    int64     `json:"parent_id"`
-	TeamID      int64     `json:"team_id"`
-	ProjectID   string    `json:"project_id"`
-	Mark        string    `json:"mark"`
-	Name        string    `json:"name"`
-	Method      string    `json:"method"`
-	URL         string    `json:"url"`
-	Request     *Request  `json:"request"`
-	Response    *Response `json:"response,omitempty"`
-	Version     int32     `json:"version"`
-	Description string    `json:"description"`
-}
+//type API struct {
+//	TargetID    int64     `json:"target_id"`
+//	ParentID    int64     `json:"parent_id"`
+//	TeamID      int64     `json:"team_id"`
+//	ProjectID   string    `json:"project_id"`
+//	Mark        string    `json:"mark"`
+//	Name        string    `json:"name"`
+//	Method      string    `json:"method"`
+//	URL         string    `json:"url"`
+//	Request     *Request  `json:"request"`
+//	Response    *Response `json:"response,omitempty"`
+//	Version     int32     `json:"version"`
+//	Description string    `json:"description"`
+//}
 
 type GetFlowReq struct {
 	SceneID int64 `form:"scene_id" binding:"required,gt=0"`
@@ -95,8 +105,11 @@ type GetFlowReq struct {
 }
 
 type GetFlowResp struct {
-	SceneID int64   `json:"scene_id"`
-	TeamID  int64   `json:"team_id"`
-	Version int32   `json:"version"`
-	Flows   []*Flow `json:"flows"`
+	SceneID int64 `json:"scene_id"`
+	TeamID  int64 `json:"team_id"`
+	Version int32 `json:"version"`
+
+	Nodes           []*Node `json:"nodes"`
+	Edges           []*Edge `json:"edges"`
+	MultiLevelNodes []byte  `json:"multi_level_nodes"`
 }
