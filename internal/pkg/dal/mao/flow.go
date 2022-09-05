adaccf4fa07eecfd8c2dcdf16225ea7377497615
liuzhichun <liuzhichun@apipost.cn>
2022-09-05 14:05:58 +0800

    refactor: optimize Node/Edge

--- internal/pkg/dal/mao/flow.go
+++ internal/pkg/dal/mao/flow.go
@@ -1,11 +1,25 @@
 package mao
 
+import (
+	"go.mongodb.org/mongo-driver/bson"
+
+	"kp-management/internal/pkg/dal/rao"
+)
+
 type Flow struct {
 	SceneID int64 `bson:"scene_id"`
 	TeamID  int64 `bson:"team_id"`
 	Version int32 `bson:"version"`
 	//Flows   string `bson:"flows"`
-	Nodes           string `bson:"nodes"`
-	Edges           string `bson:"edges"`
-	MultiLevelNodes string `bson:"multi_level_nodes"`
+	Nodes           bson.Raw `bson:"nodes"`
+	Edges           bson.Raw `bson:"edges"`
+	MultiLevelNodes bson.Raw `bson:"multi_level_nodes"`
+}
+
+type Node struct {
+	Nodes []*rao.Node `bson:"nodes"`
+}
+
+type Edge struct {
+	Edges []*rao.Edge `bson:"edges"`
 }
