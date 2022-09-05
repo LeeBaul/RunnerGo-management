885a002b70fc62205af03b690e0bab3304289a45
liuzhichun <liuzhichun@apipost.cn>
2022-09-05 11:28:20 +0800

    refactor: close request

--- internal/pkg/packer/folder.go
+++ internal/pkg/packer/folder.go
@@ -11,20 +11,20 @@ import (
 )
 
 func TransSaveFolderReqToMaoFolder(folder *rao.SaveFolderReq) *mao.Folder {
-	request, err := bson.Marshal(folder.Request)
-	if err != nil {
-		fmt.Sprintln(fmt.Errorf("folder.request json marshal err %w", err))
-	}
-
-	script, err := bson.Marshal(folder.Script)
-	if err != nil {
-		fmt.Sprintln(fmt.Errorf("folder.script json marshal err %w", err))
-	}
+	//request, err := bson.Marshal(folder.Request)
+	//if err != nil {
+	//	fmt.Sprintln(fmt.Errorf("folder.request json marshal err %w", err))
+	//}
+	//
+	//script, err := bson.Marshal(folder.Script)
+	//if err != nil {
+	//	fmt.Sprintln(fmt.Errorf("folder.script json marshal err %w", err))
+	//}
 
 	return &mao.Folder{
 		TargetID: folder.TargetID,
-		Request:  request,
-		Script:   script,
+		//Request:  request,
+		//Script:   script,
 	}
 }
 
