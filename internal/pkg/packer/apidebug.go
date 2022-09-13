182b5def01e0f8bba95804f35ad0e91e47e048cc
liuzhichun <liuzhichun@apipost.cn>
2022-09-13 10:35:07 +0800

    refactor: update apidebug

--- internal/pkg/packer/apidebug.go
+++ internal/pkg/packer/apidebug.go
@@ -1,50 +1,34 @@
 package packer
 
 import (
-	"github.com/go-omnibus/proof"
-	"go.mongodb.org/mongo-driver/bson"
-
 	"kp-management/internal/pkg/dal/mao"
 	"kp-management/internal/pkg/dal/rao"
 )
 
 func TransMaoAPIDebugToRaoAPIDebug(m *mao.APIDebug) *rao.APIDebug {
-	rawNodes, err := m.Assertion.Values()
-	if err != nil {
-		proof.Errorf("api_debug.assertion get values err", proof.WithError(err))
-	}
 
 	var as []*rao.DebugAssertion
-	for _, node := range rawNodes {
-		d, ok := node.DocumentOK()
-		if !ok {
-			proof.Errorf("api_debug.assertion DocumentOK err", proof.WithError(err))
-		}
-
-		var a rao.DebugAssertion
-		if err := bson.Unmarshal(d, &a); err != nil {
-			proof.Errorf("api_debug.assertion bson unmarshal err", proof.WithError(err))
-		}
-
-		as = append(as, &a)
+	for _, a := range m.Assertion {
+		as = append(as, &rao.DebugAssertion{
+			Code:      a.Code,
+			IsSucceed: a.IsSucceed,
+			Msg:       a.Msg,
+		})
 	}
 
-	//var regex interface{}
-	//if err := bson.Unmarshal(m.Regex, &regex); err != nil {
-	//	proof.Errorf("api_debug.regex bson unmarshal err", proof.WithError(err))
-	//}
-	//
-	//r, err := json.Marshal(regex)
-	//if err != nil {
-	//	proof.Errorf("api_debug.regex json marshal err", proof.WithError(err))
-	//}
+	var rs []*rao.DebugRegex
+	for _, r := range m.Regex {
+		rs = append(rs, &rao.DebugRegex{
+			Code: r.Code,
+		})
+	}
 
 	return &rao.APIDebug{
 		ApiID:                 m.ApiID,
 		APIName:               m.APIName,
 		Assertion:             as,
 		EventID:               m.EventID,
-		Regex:                 m.Regex.String(),
+		Regex:                 rs,
 		RequestBody:           m.RequestBody,
 		RequestCode:           m.RequestCode,
 		RequestHeader:         m.RequestHeader,
