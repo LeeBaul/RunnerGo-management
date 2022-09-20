f1464820b7721d901f0135b231f854922c1d86dd
liuzhichun <liuzhichun@apipost.cn>
2022-09-20 18:42:07 +0800

    feat: add planName to task detail

--- internal/pkg/dal/mao/report_task.go
+++ internal/pkg/dal/mao/report_task.go
@@ -1,10 +1,10 @@
 package mao
 
 type ReportTask struct {
-	UserID   int64     `json:"user_id" bson:"user_id"`
-	UserName string    `json:"user_name" bson:"user_name"`
-	ReportID int64     `json:"report_id" bson:"report_id"`
-	TaskType int32     `json:"task_type" bson:"task_type"`
-	TaskMode int32     `json:"task_mode" bson:"task_mode"`
-	ModeConf *ModeConf `json:"mode_conf" bson:"mode_conf"`
+	PlanID   int64     `bson:"plan_id"`
+	PlanName string    `bson:"plan_name"`
+	ReportID int64     `bson:"report_id"`
+	TaskType int32     `bson:"task_type"`
+	TaskMode int32     `bson:"task_mode"`
+	ModeConf *ModeConf `bson:"mode_conf"`
 }
