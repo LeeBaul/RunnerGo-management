bad6804515d87568dd82a1e4a6e66c558683e5ae
liuzhichun <liuzhichun@apipost.cn>
2022-09-27 18:30:55 +0800

    fix: fixed get api ret regex

--- internal/pkg/dal/rao/apidebug.go
+++ internal/pkg/dal/rao/apidebug.go
@@ -1,22 +1,22 @@
 package rao
 
 type APIDebug struct {
-	ApiID                 int64             `json:"api_id"`
-	APIName               string            `json:"api_name"`
-	Assertion             []*DebugAssertion `json:"assertion"`
-	EventID               string            `json:"event_id"`
-	Regex                 []*DebugRegex     `json:"regex"`
-	RequestBody           string            `json:"request_body"`
-	RequestCode           int64             `json:"request_code"`
-	RequestHeader         string            `json:"request_header"`
-	RequestTime           int64             `json:"request_time"`
-	ResponseBody          string            `json:"response_body"`
-	ResponseBytes         int64             `json:"response_bytes"`
-	ResponseHeader        string            `json:"response_header"`
-	ResponseTime          string            `json:"response_time"`
-	ResponseLen           int32             `json:"response_len"`
-	ResponseStatusMessage string            `json:"response_status_message"`
-	UUID                  string            `json:"uuid"`
+	ApiID                 int64                    `json:"api_id"`
+	APIName               string                   `json:"api_name"`
+	Assertion             []*DebugAssertion        `json:"assertion"`
+	EventID               string                   `json:"event_id"`
+	Regex                 []map[string]interface{} `json:"regex"`
+	RequestBody           string                   `json:"request_body"`
+	RequestCode           int64                    `json:"request_code"`
+	RequestHeader         string                   `json:"request_header"`
+	RequestTime           int64                    `json:"request_time"`
+	ResponseBody          string                   `json:"response_body"`
+	ResponseBytes         int64                    `json:"response_bytes"`
+	ResponseHeader        string                   `json:"response_header"`
+	ResponseTime          string                   `json:"response_time"`
+	ResponseLen           int32                    `json:"response_len"`
+	ResponseStatusMessage string                   `json:"response_status_message"`
+	UUID                  string                   `json:"uuid"`
 }
 
 type DebugAssertion struct {
