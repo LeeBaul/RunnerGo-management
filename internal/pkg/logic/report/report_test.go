461a14a445440c52de15e5355411ecd345ca351d
guosongsong <guosongsong@apipost.cn>
2022-10-14 15:10:01 +0800

    添加获取debug状态接口，并将debug状态由redis迁移到mongo

--- internal/pkg/logic/report/report_test.go
+++ internal/pkg/logic/report/report_test.go
@@ -2,10 +2,11 @@ package report
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"kp-management/internal/pkg/dal/rao"
-	"log"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"testing"
 )
 
@@ -21,13 +22,47 @@ func TestGetReportDetail(t *testing.T) {
 	//if err != nil {
 	//	panic(fmt.Sprintf("es连接失败: %s", err))
 	//}
-	var report rao.GetReportReq
-	report.ReportID = 762
-	err, result := GetReportDetail(context.Background(), report, "http://172.17.101.191:9200", "elastic", "ZSrfx4R6ICa3skGBpCdf")
-	if err != nil {
-		fmt.Println(err)
-	}
-	res, _ := json.Marshal(result)
-	log.Println(string(res))
+	//var report rao.GetReportReq
+	//report.ReportID = 762
+	//err, result := GetReportDetail(context.Background(), report, "http://172.17.101.191:9200", "elastic", "ZSrfx4R6ICa3skGBpCdf")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//res, _ := json.Marshal(result)
+	//log.Println(string(res))
+	ctx := new(gin.Context)
+	conf := fmt.Sprintf("mongodb://%s:%s@%s/%s", "kunpeng", "kYjJpU8BYvb4EJ9x", "172.17.18.255:27017", "kunpeng")
 
+	clientOptions := options.Client().ApplyURI(conf)
+	mongoClient, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return
+	}
+
+	collection := mongoClient.Database("kunpeng").Collection("api_debug")
+
+	filter := bson.D{{"uuid", "ecbbabcc-443c-46c4-abce-6170f19ca06b"}}
+	fmt.Println("lllllll        ", collection)
+	cur := collection.FindOne(context.TODO(), filter)
+	result, err := cur.DecodeBytes()
+	list, err := result.Elements()
+	for index, value := range list {
+		fmt.Println("index         ", index, " value:           ", string(value.Value().Value))
+	}
+	fmt.Println("1111111", result, " errr:           ", err)
+	//if cur == nil {
+	//	debug := bson.D{{fmt.Sprintf("%d", 123), "All"}}
+	//	_, err = collection.InsertOne(ctx, debug)
+	//	if err != nil {
+	//		response.ErrorWithMsg(ctx, errno.ErrRedisFailed, err.Error())
+	//		return
+	//	}
+	//} else {
+	//	debug := bson.D{{fmt.Sprintf("%d", 123), "all"}}
+	//	_, err = collection.UpdateMany(ctx, filter, debug)
+	//	if err != nil {
+	//		response.ErrorWithMsg(ctx, errno.ErrRedisFailed, err.Error())
+	//		return
+	//	}
+	//}
 }
