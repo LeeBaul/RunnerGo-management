9df0b0ba98edef22bcc4890b9ef7dd36112b3064
guosongsong <guosongsong@apipost.cn>
2022-10-25 15:36:39 +0800

    测试数据由es迁移到redis和mongo

--- internal/pkg/handler/report.go
+++ internal/pkg/handler/report.go
@@ -65,7 +65,7 @@ func ReportDetail(ctx *gin.Context) {
 		response.ErrorWithMsg(ctx, errno.ErrParam, err.Error())
 		return
 	}
-	err, result := report.GetReportDetail(ctx, req, nil, nil)
+	err, result := report.GetReportDetail(ctx, req)
 	if err != nil {
 		response.ErrorWithMsg(ctx, errno.ErrParam, err.Error())
 		return
