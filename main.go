f2decc58a796ff1463852e842068d8e2e8cb8e7d
wangjiawei <wangjiawei@apipost.cn>
2022-11-09 15:21:17 +0800

    优化代码

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"kp-management/internal"
 	"kp-management/internal/app/router"
 	"kp-management/internal/pkg/conf"
-	"kp-management/internal/pkg/handler"
 )
 
 func main() {
@@ -16,9 +15,9 @@ func main() {
 	router.RegisterRouter(r)
 
 	// 异步执行定时任务
-	go func() {
-		handler.TimedTaskExec()
-	}()
+	//go func() {
+	//	handler.TimedTaskExec()
+	//}()
 
 	if err := r.Run(fmt.Sprintf(":%d", conf.Conf.Http.Port)); err != nil {
 		panic(err)
